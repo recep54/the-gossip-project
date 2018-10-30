d758a10cc0aa084184ad676b885e2931016a4e66
recep54 <recep54@outlook.fr>
2018-10-30 12:09:42 +0100

    fix yarn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,22 @@
 class StaticController < ApplicationController
   def index
-    
+    # puts params[first_name]
+    puts "hellp"
+  end
+
+  def index_post
+    puts "c'est bien"
+    puts params[:first_name]
+  end
 
   def team
   end
 
   def contact
   end
+
+  def welcome
+    @prenom = params[:prenom].capitalize
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'static#index'
-  post '/', to: 'static#team'
+  post '/', to: 'static#index_post'
+  get '/index_post/:first_name', to: 'static#index_post'
   get 'team', to:  'static#team'
   get 'contact', to: 'static#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/:prenom', to: 'static#welcome'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
